68572acdfcf45d97f2040c6c2e5b438b41c67b15
SergeySofronov <nekromanser-box@yandex.ru>
2023-05-06 22:39:13 +0500

    Frontend app added

--- fit-friends/apps/frontend/webpack.config.js
+++ fit-friends/apps/frontend/webpack.config.js
@@ -1,7 +1,8 @@
 const { composePlugins, withNx } = require('@nx/webpack');
+const { withReact } = require('@nx/react');
 
 // Nx plugins for webpack.
-module.exports = composePlugins(withNx(), (config) => {
+module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
   return config;
